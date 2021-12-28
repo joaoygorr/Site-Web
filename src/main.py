8d9381fb088c59866bb61838bffa4aeb34fea4a0
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2021-12-28 13:49:55 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,11 +5,14 @@ app = Flask(__name__)
 list_user = []
 
 @app.route("/")
-def hello_world():
-    return render_template("base.html")
+def base():
+    return render_template("home.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
-@app.route("/users")
+@app.route("/user")
 def user():
     return render_template("user.html")
     
