55234d15ee3f1e86186a6f6e810c9963fff7d629
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2021-12-28 14:19:10 -0400

    Criação da pag login

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 list_user = []
 
 @app.route("/")
-def base():
+def home():
     return render_template("home.html")
 
 @app.route("/contact")
@@ -15,6 +14,10 @@ def contact():
 @app.route("/user")
 def user():
     return render_template("user.html")
+
+@app.route("/login")
+def login(): 
+    return render_template("login.html")
     
 if __name__ == "__main__": 
     app.run(debug=True)
