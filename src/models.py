9a10d955e73938b317d1bb32300585f389351875
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2021-12-29 11:41:00 -0400

    relacionamento entre as tabelas

--- src/models.py
+++ src/models.py
@@ -8,11 +8,12 @@ class User(database.Model):
     email = database.Column(database.String, nullable=False, unique=True)
     password = database.Column(database.String, nullable=False)
     profile_picture = database.Column(database.String, default='default.jpg')
+    courses = database.Column(database.String, nullable=False, default='Not Informed')
+    posts = database.relationship('Post', backred='author', lazy=True)
     
 class Post(database.Model): 
     id = database.Column(database.Integer, primary_key=True)
     title = database.Column(database.String, nullable=False)
     body = database.Column(database.Text, nullable=False)
     dt_create = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
-    
-    
+    id_user = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
