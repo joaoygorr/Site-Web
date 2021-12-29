7601bddc71bfd96af95f6c4541ab6ce7743fa62d
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2021-12-29 09:39:15 -0400

    mensagem de login e redirecionamento

--- src/forms.py
+++ src/forms.py
@@ -6,12 +6,12 @@ class FormSignIn(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(), Length(6, 20)])
     remember_me = BooleanField("Remember-me")
-    button_submit = SubmitField("Sign_In")
+    button_submitIn = SubmitField("Sign_In")
     
 class FormSignUp(FlaskForm): 
     username = StringField('Username', validators=[DataRequired(), Length(6, 20)])
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(), Length(6, 20)])
     confirm = PasswordField("Password Confirmation", validators=[DataRequired(), EqualTo('password')])
-    button_submit = SubmitField("Create Account")
+    button_submitUp = SubmitField("Create Account")
     
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormSignIn, FormSignUp
 app = Flask(__name__)
 
@@ -18,10 +18,23 @@ def contact():
 def user():
     return render_template("user.html")
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login(): 
     form_SignIn = FormSignIn()
     form_SignUp = FormSignUp()
+    
+    if form_SignIn.validate_on_submit() and 'button_submitIn' in request.form: # Login realizado
+        # Mensagem
+        flash(f"login successfully in email: {form_SignIn.email.data}", "alert-success")
+        # Redirecionando 
+        return redirect(url_for("home"))
+
+    if form_SignUp.validate_on_submit() and 'button_submitUp' in request.form:  # Conta criada
+        # Mensagem
+        flash(f"account successfully created for email: {form_SignUp.email.data}", "alert-success")
+        # Redirecionando
+        return redirect(url_for("home"))
+    
     return render_template("login.html", form_SignIn=form_SignIn, form_SignUp=form_SignUp)
     
 if __name__ == "__main__": 
