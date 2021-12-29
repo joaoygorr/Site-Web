9b7a345a46b4b8ad6277761e769c50d50fc8df63
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2021-12-29 15:16:28 -0400

    Fazendo login

--- src/site_web/models.py
+++ src/site_web/models.py
@@ -1,8 +1,14 @@
 from datetime import datetime
-from site_web import database
+from site_web import database, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(database.Model): 
+# Login
+@login_manager.user_loader 
+def load_user(id_user): 
+    return User.query.get(int(id_user))
+
+class User(database.Model, UserMixin): 
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String, nullable=False)
     email = database.Column(database.String, nullable=False, unique=True)
--- src/site_web/routes.py
+++ src/site_web/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, request, url_for, flash
 from site_web import app, database, bcrypt
 from site_web.forms import FormSignIn, FormSignUp
 from site_web.models import User
+from flask_login import login_user
 
 @app.route("/")
 def home():
@@ -21,14 +22,21 @@ def login():
     form_SignUp = FormSignUp()
     
     if form_SignIn.validate_on_submit() and 'button_submitIn' in request.form: # Login realizado
-        # Mensagem
-        flash(f"Login Successfully In Email: {form_SignIn.email.data}", "alert")
-        # Redirecionando 
-        return redirect(url_for("home"))
+        # Verificando dados se existem para login 
+        user = User.query.filter_by(email=form_SignIn.email.data).first()
+        # ERROR ValueError: Invalid Salt
+        if user and bcrypt.check_password_hash(user.password.encode('utf-8'), form_SignIn.password.data): 
+            login_user(user, remember=form_SignIn.remember_me.data) 
+            # Mensagem
+            flash(f"Login Successfully In Email: {form_SignIn.email.data}", "alert")
+            # Redirecionando 
+            return redirect(url_for("home"))
+        else: 
+            flash(f"The Email Address Or Password You Entered Is Incorrect.", "alert-danger")
 
     if form_SignUp.validate_on_submit() and 'button_submitUp' in request.form:  # Conta criada
         # Criptografando senha 
-        password_cript = bcrypt.generate_password_hash(form_SignUp.password.data)
+        password_cript = bcrypt.generate_password_hash(form_SignUp.password.data).decode('utf-8')
         # Criando usuário
         user = User(username=form_SignUp.username.data, email=form_SignUp.email.data, password=password_cript)
         database.session.add(user)
